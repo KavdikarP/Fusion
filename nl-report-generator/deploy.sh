8b455efdf2ffc3259685589bccdf2efada7ee822
KavdikarP <tpallavi23@gmail.com>
2025-07-21 17:01:50 +0530

    update logging setting

--- nl-report-generator/deploy.sh
+++ nl-report-generator/deploy.sh
@@ -3,26 +3,34 @@
 # Exit on error
 set -e
 
-PROJECT_ID=deft-clarity-461011-c7
-REGION=asia-south1
-SERVICE_NAME=cxo-prism
-DB_USER=report_user
-DB_PASS=test123
-DB_HOST=35.244.42.223
-DB_NAME=reporting_db
-BUCKET_NAME=cxo-prism
+export PROJECT_ID=deft-clarity-461011-c7
+export CLOUDRUN_SERVICE_NAME=cxo-prism
+export CLOUDRUN_SERVICE_IMAGE_NAME=asia-south1-docker.pkg.dev/$PROJECT_ID/$CLOUDRUN_SERVICE_NAME/$CLOUDRUN_SERVICE_NAME:latest
+export REGION=asia-south1
+export DB_USER=report_user
+export DB_PASS=test123
+export DB_HOST=35.244.42.223
+export DB_NAME=reporting_db
+export BUCKET_NAME=cxo-prism
 
-# Build Docker image and push to Google Container Registry
-gcloud builds submit --tag gcr.io/$PROJECT_ID/$SERVICE_NAME
+# Setup
+gcloud config set project $PROJECT_ID
+gcloud auth application-default set-quota-project $PROJECT_ID
+
+
+# Build image
+gcloud builds submit --tag $CLOUDRUN_SERVICE_IMAGE_NAME .
 
 # Deploy to Cloud Run
 gcloud run deploy $SERVICE_NAME \
-  --image gcr.io/$PROJECT_ID/$SERVICE_NAME \
+  --image $CLOUDRUN_SERVICE_IMAGE_NAME \
   --region $REGION \
   --port 8502 --allow-unauthenticated
   --set-env-vars BUCKET_NAME=$BUCKET_NAME,PROJECT_ID=$PROJECT_ID,REGION=$REGION,DB_USER=$DB_USER,DB_PASS=$DB_PASS,DB_HOST=$DB_HOST,DB_NAME=$DB_NAME
 
 echo "Deployment completed. Access your service via the Cloud Run console."
+
+
 
 
 
