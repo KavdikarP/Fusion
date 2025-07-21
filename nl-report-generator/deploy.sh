69725147c4d64c39265f7a9a54fbe46e8d2d0c04
KavdikarP <tpallavi23@gmail.com>
2025-07-21 15:41:14 +0530

    updated deploy.sh

--- nl-report-generator/deploy.sh
+++ nl-report-generator/deploy.sh
@@ -10,6 +10,7 @@ DB_USER=report_user
 DB_PASS=test123
 DB_HOST=35.244.42.223
 DB_NAME=reporting_db
+BUCKET_NAME=cxo-prism
 
 # Build Docker image and push to Google Container Registry
 gcloud builds submit --tag gcr.io/$PROJECT_ID/$SERVICE_NAME
@@ -17,9 +18,11 @@ gcloud builds submit --tag gcr.io/$PROJECT_ID/$SERVICE_NAME
 # Deploy to Cloud Run
 gcloud run deploy $SERVICE_NAME \
   --image gcr.io/$PROJECT_ID/$SERVICE_NAME \
-  --platform managed \
   --region $REGION \
-  --allow-unauthenticated \
-  --set-env-vars BUCKET_NAME=your-gcs-bucket,PROJECT_ID=$PROJECT_ID,REGION=$REGION,DB_USER=$DB_USER,DB_PASS=$DB_PASS,DB_HOST=$DB_HOST,DB_NAME=$DB_NAME
+  --port 8502 --allow-unauthenticated
+  --set-env-vars BUCKET_NAME=$BUCKET_NAME,PROJECT_ID=$PROJECT_ID,REGION=$REGION,DB_USER=$DB_USER,DB_PASS=$DB_PASS,DB_HOST=$DB_HOST,DB_NAME=$DB_NAME
 
 echo "Deployment completed. Access your service via the Cloud Run console."
+
+
+
